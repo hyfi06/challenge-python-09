b82c9098e3b3a84fe8758cfca262a08e4a79ce70
Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>
2020-05-21 18:24:07 -0500

    style: fix pylint indcations

--- solutions/hyfi06/solution.py
+++ solutions/hyfi06/solution.py
@@ -1,17 +1,20 @@
+"""Solution module"""
 from typing import List
 
 
 class Solution:
+    """Class solution of challenge """
 
     def duplicate_zeros(self, l: List[int]):
+        """Duplicate the zeros in a list"""
         insert = 1
         list_length = len(l)
         for index, item in enumerate(l):
-            if (index >= list_length):
+            if index >= list_length:
                 break
-            if (item == 0):
-                if(insert):
+            if item == 0:
+                if insert:
                     l.insert(index, 0)
                 insert = 1 - insert
-        for _ in range(list_length,len(l)):
+        for _ in range(list_length, len(l)):
             l.pop()
