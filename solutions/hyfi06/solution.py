cb6c4b9cfac778b408626ce67abe7624c6b83da2
Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>
2020-05-21 21:50:54 -0500

    other solution

--- solutions/hyfi06/solution.py
+++ solutions/hyfi06/solution.py
@@ -5,16 +5,51 @@ from typing import List
 class Solution:
     """Class solution of challenge """
 
-    def duplicate_zeros(self, l: List[int]):
+    def duplicate_zeros(self, arr: List[int]):
         """Duplicate the zeros in a list"""
-        insert = 1
-        list_length = len(l)
-        for index, item in enumerate(l):
-            if index >= list_length:
-                break
+        zeros = 0
+        last_index = 0
+
+        arr_len = len(arr)
+        for index, item in enumerate(arr):
             if item == 0:
-                if insert:
-                    l.insert(index, 0)
-                insert = 1 - insert
-        for _ in range(list_length, len(l)):
-            l.pop()
+                if index + zeros + 1 < arr_len:
+                    zeros += 1
+                    last_index = index
+                else:
+                    break
+
+        index = arr_len - 1
+
+        print(f'{"="*5}{arr}:{zeros}{"="*5}')
+        while zeros > 0 and index - zeros >= 0:
+            print(f'{index}: {arr} zeros:  {zeros}')
+            print(f'|-- {index - zeros}:{arr[index - zeros]} => {index}:{arr[index]}')
+            arr[index] = arr[index - zeros]
+
+            if arr[index - zeros] == 0 and index - zeros <= last_index:
+                arr[index - 1] = 0
+                zeros -= 1
+                index -= 1
+            index -= 1
+        print(f'>: {arr}')
+        print('\n\n')
+
+
+def run():
+    solution = Solution()
+    solution.duplicate_zeros(list([0]))
+    solution.duplicate_zeros(list([0, 0]))
+    solution.duplicate_zeros(list([1, 0]))
+    solution.duplicate_zeros(list([0, 1]))
+    solution.duplicate_zeros(list([1, 1]))
+    solution.duplicate_zeros(list([1, 0, 0, 0, 0]))
+    solution.duplicate_zeros(list([0, 1, 1, 0, 1, 1, 1]))
+    solution.duplicate_zeros(list([0, 1, 0, 0, 1, 0, 0, 3]))
+    solution.duplicate_zeros(list([0, 1, 0, 1, 0, 1, 0]))
+    solution.duplicate_zeros(list([0, 0, 1, 1, 1, 0, 0, 0]))
+    solution.duplicate_zeros(list([0, 1, 0, 0, 1, 0, 0, 1, 0]))
+
+
+if (__name__ == "__main__"):
+    run()
