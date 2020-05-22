434140b320bd2805617cb319b1debfb4d19e3ec8
Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>
2020-05-22 03:13:57 -0500

    style: fix pylint indication x2

--- solutions/hyfi06/solution.py
+++ solutions/hyfi06/solution.py
@@ -21,10 +21,7 @@ class Solution:
 
         index = arr_len - 1
 
-        print(f'{"="*5}{arr}:{zeros}{"="*5}')
         while zeros > 0 and index - zeros >= 0:
-            print(f'{index}: {arr} zeros:  {zeros}')
-            print(f'|-- {index - zeros}:{arr[index - zeros]} => {index}:{arr[index]}')
             arr[index] = arr[index - zeros]
 
             if arr[index - zeros] == 0 and index - zeros <= last_index:
@@ -32,24 +29,3 @@ class Solution:
                 zeros -= 1
                 index -= 1
             index -= 1
-        print(f'>: {arr}')
-        print('\n\n')
-
-
-def run():
-    solution = Solution()
-    solution.duplicate_zeros(list([0]))
-    solution.duplicate_zeros(list([0, 0]))
-    solution.duplicate_zeros(list([1, 0]))
-    solution.duplicate_zeros(list([0, 1]))
-    solution.duplicate_zeros(list([1, 1]))
-    solution.duplicate_zeros(list([1, 0, 0, 0, 0]))
-    solution.duplicate_zeros(list([0, 1, 1, 0, 1, 1, 1]))
-    solution.duplicate_zeros(list([0, 1, 0, 0, 1, 0, 0, 3]))
-    solution.duplicate_zeros(list([0, 1, 0, 1, 0, 1, 0]))
-    solution.duplicate_zeros(list([0, 0, 1, 1, 1, 0, 0, 0]))
-    solution.duplicate_zeros(list([0, 1, 0, 0, 1, 0, 0, 1, 0]))
-
-
-if (__name__ == "__main__"):
-    run()
